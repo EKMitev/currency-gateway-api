ffb7fcb09696189ab8bdcab414fafedfa570d3e1
ekmitev <ekmitev@gmail.com>
2024-09-13 15:37:04 +0300

    Commit project

---
+++ src/main/java/com/example/Currency_Gateway_API/CurrencyGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Currency_Gateway_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyGatewayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Currency_Gateway_API/CurrencyGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Currency_Gateway_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
